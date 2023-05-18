b83b1add27c6d121037b8ab77ed9c6f89f908414
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-18 15:14:04 +0200

    add appheader compnent

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
-<script></script>
+<script>
+import AppHeader from "./components/AppHeader.vue";
 
-<template></template>
+export default {
+  components: {
+    AppHeader,
+  },
+};
+</script>
+
+<template>
+  <AppHeader />
+</template>
 
 <style lang="sass"></style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,18 @@
 <script>
 export default {};
 </script>
-<template></template>
+<template>
+  <header>
+    <div class="navbar">
+      <div class="logo">
+        <img src="../assets/img/dc-logo.png" alt="Dc Logo" />
+      </div>
+    </div>
+  </header>
+</template>
 
-<style lang="sass" scoped></style>
+<style lang="scss" scoped>
+.logo {
+  display: flex;
+}
+</style>
