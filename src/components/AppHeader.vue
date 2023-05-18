77affd9cd1b98e73d8e933039805130231bc704e
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-18 15:28:12 +0200

    add choses

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,6 +7,21 @@ export default {};
       <div class="logo">
         <img src="../assets/img/dc-logo.png" alt="Dc Logo" />
       </div>
+
+      <div class="choose">
+        <ul>
+          <li><a>Characters</a></li>
+          <li><a>Comics</a></li>
+          <li><a>Movies</a></li>
+          <li><a>TV</a></li>
+          <li><a>Games</a></li>
+          <li><a>Collectibles</a></li>
+          <li><a>Videos</a></li>
+          <li><a>Fans</a></li>
+          <li><a>News</a></li>
+          <li><a>Shop</a></li>
+        </ul>
+      </div>
     </div>
   </header>
 </template>
@@ -14,5 +29,6 @@ export default {};
 <style lang="scss" scoped>
 .logo {
   display: flex;
+  justify-content: space-between;
 }
 </style>
