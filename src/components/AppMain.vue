47783edf9986532cb0f0dea9179d3436411407be
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-18 15:47:54 +0200

    complete appmain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,22 @@
+<script>
+export default {};
+</script>
+<template>
+  <main>
+    <div class="main-content">Contents goes here!</div>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+@use "../assets/styles/partials/variables" as *;
+main {
+  background-color: $black;
+  height: 200px;
+  color: $white;
+  font-size: 2rem;
+  font-weight: 600;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
