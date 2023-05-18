2a9c5a4525c83f93be440776ca8f1ce97fd2e40d
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-05-18 16:29:25 +0200

    complete first option in footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -34,7 +34,37 @@ export default {};
   </div>
 
   <div class="bottom-footer">
-    <div class="container"></div>
+    <div class="container">
+      <div class="footer-links">
+        <ul>
+          <li class="option">
+            DC COMICS
+            <ul>
+              <li class="sub-option">Character</li>
+              <li class="sub-option">Comics</li>
+              <li class="sub-option">Movies</li>
+              <li class="sub-option">TV</li>
+              <li class="sub-option">Games</li>
+              <li class="sub-option">Videos</li>
+              <li class="sub-option">News</li>
+            </ul>
+          </li>
+
+          <li class="option">
+            DC
+            <ul>
+              <li class="sub-option">Character</li>
+              <li class="sub-option">Comics</li>
+              <li class="sub-option">Movies</li>
+              <li class="sub-option">TV</li>
+              <li class="sub-option">Games</li>
+              <li class="sub-option">Videos</li>
+              <li class="sub-option">News</li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -70,5 +100,15 @@ export default {};
 .bottom-footer {
   background-image: url(../assets/img/footer-bg.jpg);
   background-size: cover;
+}
+
+.option {
+  color: $white;
+  font-size: 1rem;
+  font-weight: 700;
+}
+.sub-option {
+  color: $gray;
+  font-size: 0.6rem;
 }
 </style>
